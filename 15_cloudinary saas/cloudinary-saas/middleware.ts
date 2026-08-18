c3c55574fe527affeb6a97eb201e8582ec50d3a4
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-08-19 01:06:28 +0530

    initial project setup and added configure middleware for clerk auth

---
+++ 15_cloudinary saas/cloudinary-saas/middleware.ts
@@ -0,0 +1,50 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/signin(.*)",
+  "/signup(.*)",
+  "/",
+]);
+
+const isPublicApiRoute = createRouteMatcher([
+  "/api/videos(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+  const currentUrl = new URL(req.url);
+  const isAccessingDashboard = currentUrl.pathname === "/home";
+  const isApiRequest = currentUrl.pathname.startsWith("/api");
+
+  // If user is logged in and trying to access a public route (e.g. signin/signup/landing)
+  if (userId && isPublicRoute(req) && !isAccessingDashboard) {
+    return NextResponse.redirect(new URL("/home", req.url));
+  }
+
+  // If user is not logged in and attempting to access a protected route
+  if (!userId) {
+    // Protected API routes
+    if (isApiRequest && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL("/signin", req.url));
+    }
+
+    // Protected page routes
+    if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL("/signin", req.url));
+    }
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+    // Always run for Clerk-specific frontend API routes
+    "/__clerk/(.*)",
+  ],
+};
